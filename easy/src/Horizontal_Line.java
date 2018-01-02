8dbaa0eef5ff931b784e17c7af040cadf2d8c6ff
Jean Bauer <jeanbauerc@gmail.com>
2018-01-02 17:02:32 -0200

    Add Horizontal_Line exercise

---
+++ easy/src/Horizontal_Line.java
@@ -0,0 +1,12 @@
+public class Horizontal_Line {
+  public static void main(String[] args) {
+      String line = "";
+      int lineLength = 8;
+
+      for (int i = 0; i < lineLength; i++) {
+          line += "*";
+      }
+
+      System.out.println(line);
+  }
+}
