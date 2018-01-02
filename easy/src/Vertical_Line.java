dc489f38fdd7019dd49386046958bb02bf9809f8
Jean Bauer <jeanbauerc@gmail.com>
2018-01-02 17:04:26 -0200

    Add Vertical_Line exercise

---
+++ easy/src/Vertical_Line.java
@@ -0,0 +1,8 @@
+public class Vertical_Line {
+    public static void main(String[] args) {
+        int rows = 3;
+        for (int i = 0; i < rows; i++) {
+            System.out.println("*");
+        }
+    }
+}
