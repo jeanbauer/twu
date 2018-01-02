e680bc007c2256cad6d3222121f037e45cebcde0
Jean Bauer <jeanbauerc@gmail.com>
2018-01-02 18:01:53 -0200

    Add Isosceles Triangle

---
+++ easy/src/Isosceles_Triangle.java
@@ -0,0 +1,16 @@
+public class Isosceles_Triangle {
+    public static void main(String[] args) {
+        int n = 3;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = n - 1; j >= i; j--) {
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j <= (2*i-1); j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
