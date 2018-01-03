e7bb391c91a6560654d8a0c86970f368ed213af5
Jean Bauer <jeanbauerc@gmail.com>
2018-01-03 09:02:28 -0200

    Add Diamond solution

--- easy/src/Diamond.java
+++ easy/src/Diamond.java
@@ -1,6 +1,7 @@
+
 public class Diamond {
     public static void main(String[] args) {
-        int n = 3;
+        int n = 6;
 
         for(int i = 1; i <= n; i++) {
             for(int j = n - 1; j >= i; j--) {
@@ -10,10 +11,19 @@ public class Diamond {
             for(int j = 1; j <= (2*i-1); j++){
                 System.out.print("*");
             }
+            System.out.println("");
+        }
+
+        for(int i = n - 1; i > 0; i--) {
+
+            for(int j = n - 1; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= (2*i-1); j++){
+                System.out.print("*");
+            }
 
             System.out.println("");
-
-            // WIP
         }
     }
 }
