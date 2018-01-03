3bafa6b054490796061ed88597a80f812adbcd35
Jean Bauer <jeanbauerc@gmail.com>
2018-01-03 14:27:06 -0200

    Add gitignore

---
+++ tests/com/twu/biblioteca/FizzBuzzWriterTest.java
@@ -0,0 +1,19 @@
+package com.twu.biblioteca;
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void printFizzWhenNumberIsThree() throws Exception {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        assertEquals(0, fizzBuzz.);
+    }
+}
+
+/*
+Instead of numbers divisible by three print "Fizz".
+Instead of numbers divisible by five print "Buzz".
+Instead of numbers divisible by three and five print "FizzBuzz".
+ */
